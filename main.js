22fbe7b043f7194a2c5a9fa3416b3a3e2e9f7b18
Andrei Bucur <andrei.bucur@recomedica.com>
2022-06-18 20:19:58 +0300

    da

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ input.addEventListener('keypress', function(e){
 
 function getPokemon(){
     let inputValue = input.value;
-    const req = fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`).then(pokemon => pokemon.json()).then(data => showPokemon(data));
+    const req = fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`).then(pokemon => pokemon.json()).then(data => inputValue ? showPokemon(data) : null);
 
         console.log(req);
 }
