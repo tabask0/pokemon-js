2d76b3ace20084af9cd1e8b7ea32390b9ffe0316
Andrei Bucur <andrei.bucur@recomedica.com>
2022-06-18 20:02:17 +0300

    da

--- main.js
+++ main.js
@@ -7,9 +7,8 @@ const input = document.getElementById('input');
 
 function getPokemon(){
     let inputValue = input.value;
-    const req = fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`).then(pokemon => pokemon.json()).then(data => {return data});
-
-        console.log(req);
-}
+    const req = fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`).then(pokemon => pokemon.json()).then(data => console.log(data));
+    return req
+  }
 
 
