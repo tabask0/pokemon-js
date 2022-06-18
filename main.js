b65a1735b8a6ef342a2d25cd43208babf094382c
unknown <Ingrid0802>
2022-06-18 18:51:55 +0300

    da

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-const req = fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(poke => poke.json()).then(data => console.log(data))
+const button = document.getElementById('button');
+button.addEventListener('click', getPokemon);
+
+const input = document.getElementById('input');
+
+
+
+function getPokemon(){
+    let inputValue = input.value;
+    const req = fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`).then(pokemon => pokemon.json()).then(data => {return data});
+
+        console.log(req);
+}
+
+
